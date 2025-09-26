567628e8fb27e0d56812eee0127e438b93ca302f
prathmeshrai0 <jatinrai862000@gmail.com>
2025-09-26 19:38:03 +0530

    making live after final updates side bar updated

--- components/Navbar.js
+++ components/Navbar.js
@@ -78,7 +78,7 @@ const Navbar = () => {
               </Link>
             )}
 
-            <SelectMore option={   ['Upcoming Updates', 'About Us' ,'Theme', 'Report Issue'] } />
+            <SelectMore option={['Upcoming Updates', 'About Us', 'Theme', 'Report Issue']} />
           </ul>
           <div className="buttons flex gap-3   justify-center  items-center sm:justify-end     w-min    ">
             {session?.user ? (
@@ -164,16 +164,10 @@ const Navbar = () => {
             className={`fixed  top-14 z-10  -right-[5%]  w-screen    bg-white h-screen  md:hidden     `}
           >
             <div className="  flex   items-center      gap-5 mt-5    justify-center      flex-col   ">
-              <Link
-                className="hover:bg-gray-200 lg:px-5 px-1 py-1.5 rounded-sm whitespace-nowrap"
-                href="/"
-                onClick={toggleSideBar}
-              >
-                Home
-              </Link>
+
 
               <Link
-                className="hover:bg-gray-200 lg:px-5 px-1 py-1.5 rounded-sm whitespace-nowrap"
+                className="hover:bg-gray-200 lg:px-5 px-1 py-1.5 rounded-sm whitespace-nowrap "
                 href="/upcoming-updates"
                 onClick={toggleSideBar}
               >
@@ -181,7 +175,7 @@ const Navbar = () => {
               </Link>
 
               <Link
-                className="hover:bg-gray-200 lg:px-5 px-1 py-1.5 rounded-sm whitespace-nowrap"
+                className="hover:bg-gray-200 lg:px-5 px-1 py-1.5 rounded-sm whitespace-nowrap "
                 href="/about-us"
                 onClick={toggleSideBar}
               >
@@ -189,7 +183,7 @@ const Navbar = () => {
               </Link>
 
               <Link
-                className="hover:bg-gray-200 lg:px-5 px-1 py-1.5 rounded-sm whitespace-nowrap"
+                className="hover:bg-gray-200 lg:px-5 px-1 py-1.5 rounded-sm whitespace-nowrap "
                 onClick={() => {
                   UnderDevelopmentFeature();
                   toggleSideBar();
@@ -198,9 +192,10 @@ const Navbar = () => {
               >
                 Theme
               </Link>
+
               {ifUserInfoAvailable && (
                 <Link
-                  className="hover:bg-gray-200 lg:px-5 px-1 py-1.5 rounded-sm whitespace-nowrap"
+                  className="hover:bg-gray-200 lg:px-5 px-1 py-1.5 rounded-sm whitespace-nowrap "
                   href="/update-info"
                   onClick={() => {
                     toggleSideBar();
@@ -209,6 +204,28 @@ const Navbar = () => {
                   Update Info
                 </Link>
               )}
+
+              <Link
+                className="hover:bg-gray-200 lg:px-5 px-1 py-1.5 rounded-sm whitespace-nowrap flex   items-center justify-center  "
+                href="/report-issue"
+                onClick={toggleSideBar}
+              >
+                Report Issue  <svg
+                        className="w-4 h-4 text-yellow-500 ml-1"
+                        fill="none"
+                        stroke="currentColor"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                        aria-hidden="true"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={2}
+                            d="M10.29 3.86L1.82 18a1 1 0 0 0 .86 1.5h18.64a1 1 0 0 0 .86-1.5L13.71 3.86a1 1 0 0 0-1.72 0zM12 9v4m0 4h.01"
+                        />
+                    </svg>
+              </Link>
             </div>
           </div>
         )}
